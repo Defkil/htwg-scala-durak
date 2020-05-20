e21f17046a1e5729b43a90e7006d9f2c5aea5503
Oliver <dev@olivergruettner.de>
2020-05-20 17:48:28 +0200

    CardSpec transform to WordSpec

--- src/test/scala/de/htwg/se/durak/model/CardStackSpc.scala
+++ src/test/scala/de/htwg/se/durak/model/CardStackSpc.scala
@@ -6,15 +6,20 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class CardStackSpc extends WordSpec with Matchers {
-  val stack = new CardStack(6)
-  val cards = List(new Card(9,3),new Card(2,4),new Card(11,1))
-  for(c <- cards) stack.addCard(c)
+  "A Card" when { "new" should {
+    "check adding and removing 3 cards to a stack"  in {
+      val stack = new CardStack(6)
+      val cards = List(new Card(9,3),new Card(2,4),new Card(11,1))
+      for(c <- cards) stack.addCard(c)
 
-  stack.getSize should be(3)
-  val c1 = stack.popCard()
-  c1.toString() should be("Rang: Bube, Typ: Pik")
-  val c2 = stack.popCard()
-  c2.toString() should be("Rang: 2, Typ: Herz")
-  val c3 = stack.popCard()
-  c3.toString() should be("Rang: 9, Typ: Kreuz")
+      stack.getSize should be(3)
+      val c1 = stack.popCard()
+      c1.toString() should be("Rang: Bube, Typ: Pik")
+      val c2 = stack.popCard()
+      c2.toString() should be("Rang: 2, Typ: Herz")
+      val c3 = stack.popCard()
+      c3.toString() should be("Rang: 9, Typ: Kreuz")
+    }
+  }}
 }
+
