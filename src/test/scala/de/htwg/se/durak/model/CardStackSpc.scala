74bc2200aa1bb3a2561ebd4332a82a6d18ad55de
Oliver <dev@olivergruettner.de>
2020-05-20 17:52:44 +0200

    CardSpec transform to WordSpec

--- src/test/scala/de/htwg/se/durak/model/CardStackSpc.scala
+++ src/test/scala/de/htwg/se/durak/model/CardStackSpc.scala
@@ -7,29 +7,26 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class CardStackSpc extends WordSpec with Matchers {
   "A Card" when {
-    "new" should {
-      "check adding and removing 3 cards to a stack" in {
-        val stack = new CardStack(6)
-        val cards = List(new Card(9, 3), new Card(2, 4), new Card(11, 1))
-        for (c <- cards) stack.addCard(c)
+    "check adding and removing 3 cards to a stack" should {
+      val stack = new CardStack(6)
+      val cards = List(new Card(9, 3), new Card(2, 4), new Card(11, 1))
+      for (c <- cards) stack.addCard(c)
 
-        "CardStack is working like a stack" when {
-          "size is amount of pushed Cards" in {
-            stack.getSize should be(3)
-          }
-          "first pop ist last push" in {
-            val c1 = stack.popCard()
-            c1.toString() should be("Rang: Bube, Typ: Pik")
-          }
-          "second pop is second push" in {
-            val c2 = stack.popCard()
-            c2.toString() should be("Rang: 2, Typ: Herz")
-          }
-          "last pop ist first push" in {
-            val c3 = stack.popCard()
-            c3.toString() should be("Rang: 9, Typ: Kreuz")
-          }
-        }
+      "size is amount of pushed Cards" in {
+        stack.getSize should be(3)
+      }
+      "first pop ist last push" in {
+        val c1 = stack.popCard()
+        c1.toString() should be("Rang: Bube, Typ: Pik")
+      }
+      "second pop is second push" in {
+        val c2 = stack.popCard()
+        c2.toString() should be("Rang: 2, Typ: Herz")
+      }
+      "last pop ist first push" in {
+        val c3 = stack.popCard()
+        c3.toString() should be("Rang: 9, Typ: Kreuz")
       }
     }
-  }}
+  }
+}
