a3873f5128e0a7233029d94fd1e9334da2d7e712
Oliver <dev@olivergruettner.de>
2020-05-20 17:21:08 +0200

    CardSpec changed test style

--- src/test/scala/de/htwg/se/durak/model/CardSpec.scala
+++ src/test/scala/de/htwg/se/durak/model/CardSpec.scala
@@ -6,23 +6,13 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class CardSpec extends WordSpec with Matchers {
-  val cards = Array.ofDim[Card](52)
-  var index = 0
-  for(i <- 2 to 14) {
-    for (j <- 1 to 4) {
-      cards(index) = new Card(i,j)
-      index += 1
+  "A Card" when { "new" should {
+    val card = new Card(2,1)
+    "have get(): (rank, cardType)"  in {
+      card.get() should be(2,1)
     }
-  }
-
-  val ranks = List("2", "3", "4", "5", "6", "7", "8", "9", "10", "Bube", "Dame", "König", "Ass")
-  val cardTypes = List("Pik", "Karo", "Kreuz", "Herz")
-
-  var index2 = 0
-  for(i <- 0 to 12) {
-    for (j <- 0 to 3) {
-      cards(index2).toString should be("Rang: " + ranks(i) + ", Typ: " + cardTypes(j))
-      index2 += 1
+    "have toString with words"  in {
+      card.toString should be("Rang: 2, Typ: Pik")
     }
-  }
+  }}
 }
