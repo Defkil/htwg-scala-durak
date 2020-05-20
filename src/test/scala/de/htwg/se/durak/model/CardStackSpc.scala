28f05a017305498807d36a4ce52dd47a7104b116
Johannes <39rasenmaeher78>
2020-05-20 18:09:22 +0200

    testing method() popCards in Class CardStack

--- src/test/scala/de/htwg/se/durak/model/CardStackSpc.scala
+++ src/test/scala/de/htwg/se/durak/model/CardStackSpc.scala
@@ -6,7 +6,7 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class CardStackSpc extends WordSpec with Matchers {
-  "A Card" when {
+  "A CardStack" when {
     "check adding and removing 3 cards to a stack" should {
       val stack = new CardStack(6)
       val cards = List(new Card(9, 3), new Card(2, 4), new Card(11, 1))
@@ -26,6 +26,19 @@ class CardStackSpc extends WordSpec with Matchers {
       "last pop ist first push" in {
         val c3 = stack.popCard()
         c3.toString() should be("Rang: 9, Typ: Kreuz")
+      }
+    }
+    "Adding two Cards and removing both bei call popCards() mit argument 2" should {
+      val stack = new CardStack(48)
+      "Adding two cars" in {
+        stack.addCard(new Card(2, 3))
+        stack.addCard(new Card(11, 1))
+      }
+      "popping two cards in one method call" in {
+        val cards = stack.popCards(2)
+      }
+      "the stack should be empty" in {
+        stack.getSize should be(0)
       }
     }
   }
