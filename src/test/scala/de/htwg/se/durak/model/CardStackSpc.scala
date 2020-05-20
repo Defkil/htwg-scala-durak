5c2dbc67df905e0bebe377db19f99ddccc851f62
Oliver <dev@olivergruettner.de>
2020-05-20 17:07:38 +0200

    CardStackSpc file

---
+++ src/test/scala/de/htwg/se/durak/model/CardStackSpc.scala
@@ -0,0 +1,10 @@
+package de.htwg.se.durak.model
+
+import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class CardStackSpc extends WordSpec with Matchers {
+  val c = new CardStack(6)
+}
