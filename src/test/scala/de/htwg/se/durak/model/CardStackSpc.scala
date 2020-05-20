d400a6a0163f09b342a8456ad6e7fdef154cf050
Johannes <39rasenmaeher78>
2020-05-20 17:46:28 +0200

    WordSpecs in Test-Class CardStackSpc added.

--- src/test/scala/de/htwg/se/durak/model/CardStackSpc.scala
+++ src/test/scala/de/htwg/se/durak/model/CardStackSpc.scala
@@ -10,11 +10,21 @@ class CardStackSpc extends WordSpec with Matchers {
   val cards = List(new Card(9,3),new Card(2,4),new Card(11,1))
   for(c <- cards) stack.addCard(c)
 
-  stack.getSize should be(3)
-  val c1 = stack.popCard()
-  c1.toString() should be("Rang: Bube, Typ: Pik")
-  val c2 = stack.popCard()
-  c2.toString() should be("Rang: 2, Typ: Herz")
-  val c3 = stack.popCard()
-  c3.toString() should be("Rang: 9, Typ: Kreuz")
+  "CardStack is working like a stack" when {
+    "size is amount of pushed Cards" in {
+      stack.getSize should be(3)
+    }
+    "first pop ist last push" in {
+      val c1 = stack.popCard()
+      c1.toString() should be("Rang: Bube, Typ: Pik")
+    }
+    "second pop is second push" in {
+      val c2 = stack.popCard()
+      c2.toString() should be("Rang: 2, Typ: Herz")
+    }
+    "last pop ist first push" in {
+      val c3 = stack.popCard()
+      c3.toString() should be("Rang: 9, Typ: Kreuz")
+    }
+  }
 }
