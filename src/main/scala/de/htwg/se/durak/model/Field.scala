8d550ef3ace5a3d923a3282a13850918d000e9fc
Oliver <dev@olivergruettner.de>
2020-05-29 11:43:38 +0200

    fixed field

--- src/main/scala/de/htwg/se/durak/model/Field.scala
+++ src/main/scala/de/htwg/se/durak/model/Field.scala
@@ -3,30 +3,27 @@ package de.htwg.se.durak.model
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
-case class Field(var fieldList:ListBuffer[Card]) {
+case class Field(var stack: CardStack) {
   def this() {
-    this(new ListBuffer[Card]());
+    this(new CardStack());
   }
   val size = 0
 
-  def addAttack(card: Card): Field = {
-    fieldList += card
-    Field(fieldList)
-  }
+  def addAttack(card: Card): ListBuffer[Card] = stack.addCard(card)
 
-  def getData(): List[CardStack] = {
-    if(fieldList.isEmpty) return null
+  /*def getData(): List[CardStack] = {
+    if(stack.getSize == 0) return null
     var res = new ListBuffer[CardStack]()
 
-    for(i <- 0 to fieldList.length by 2) {
-      if(fieldList.length % 2 == 0) {
-          res += CardStack(2, ListBuffer(res(i), res(i + 1)))
+    for(i <- 0 to stack.getSize by 2) {
+      if(stack.getSize  % 2 == 0) {
+          res += CardStack(2, ListBuffer(fieldList(i), fieldList(i + 1)))
       } else {
-          val temp = if(fieldList.length < i) null else fieldList(i)
-          res += CardStack(2, ListBuffer(res(i), temp))
+          val temp = if(stack.getSize  < i) null else fieldList(i)
+          res += CardStack(2, ListBuffer(fieldList(i), temp))
       }
     }
     res.toList
-  }
+  }*/
 
 }
