8a17c2e9576332e919c0012d997ad58b2fbc743f
Oliver <dev@olivergruettner.de>
2020-06-19 06:10:54 +0200

    CardStackFactorySpc

---
+++ src/test/scala/de/htwg/se/durak/controller/CardStackFactorySpc.scala
@@ -0,0 +1,21 @@
+package de.htwg.se.durak.controller
+
+import org.scalatest._
+import org.junit.runner.{Runner, RunWith}
+import org.scalatest.matchers.should.Matchers
+
+@RunWith(classOf[Runner])
+class CardStackFactorySpc extends WordSpec with Matchers {
+  "CardStackFactory" should {
+    val cardStackFactory = new CardStackFactory
+    "generate player stack" in {
+      cardStackFactory.getInstance(0).size should be(0)
+    }
+    "generate playing stack" in {
+      cardStackFactory.getInstance(1).size should be(0)
+    }
+    "generate outside stack" in {
+      cardStackFactory.getInstance(2).size should be(0)
+    }
+  }
+}
