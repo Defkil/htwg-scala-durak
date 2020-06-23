e76062f00e4c0e8a6d585e82cf23e13184f85802
Oliver <dev@olivergruettner.de>
2020-06-24 01:49:18 +0200

    coveralls.io fix

--- src/test/scala/de/htwg/se/durak/controller/RoundFactorySpec.scala
+++ src/test/scala/de/htwg/se/durak/controller/RoundFactorySpec.scala
@@ -1,6 +1,5 @@
 package de.htwg.se.durak.controller
 
-import de.htwg.se.durak.model.RoundData
 import org.scalatest._
 import org.junit.runner.{RunWith, Runner}
 import org.scalatest.matchers.should.Matchers
@@ -10,67 +9,67 @@ import org.scalatest.matchers.should.Matchers
 class RoundFactorySpec  extends WordSpec with Matchers {
   "An RoundFactorySpec" should {
     "default routes -1" in {
-      val roundFactory = new RoundFactory
-      var temp = roundFactory.getInstance(-1, None)
+      val roundFactory = RoundFactory()
+      val temp = roundFactory.getInstance(-1, None)
       temp.siteID should be(-1)
       temp.param should be(None)
     }
     "default routes 0" in {
-      val roundFactory = new RoundFactory
-      var temp = roundFactory.getInstance(0, None)
+      val roundFactory = RoundFactory()
+      val temp = roundFactory.getInstance(0, None)
       temp.siteID should be(0)
       temp.param should be(None)
     }
     "default routes 1" in {
-      val roundFactory = new RoundFactory
-      var temp = roundFactory.getInstance(1, None)
+      val roundFactory = RoundFactory()
+      val temp = roundFactory.getInstance(1, None)
       temp.siteID should be(1)
       temp.param should be(None)
     }
     "default routes 2" in {
-      val roundFactory = new RoundFactory
-      var temp = roundFactory.getInstance(2, None)
+      val roundFactory = RoundFactory()
+      val temp = roundFactory.getInstance(2, None)
       temp.siteID should be(2)
       temp.param should be(None)
     }
     "default routes 3" in {
-      val roundFactory = new RoundFactory
-      var temp = roundFactory.getInstance(3, None)
+      val roundFactory = RoundFactory()
+      val temp = roundFactory.getInstance(3, None)
       temp.siteID should be(3)
       temp.param should be(None)
     }
     "default routes 10" in {
-      val roundFactory = new RoundFactory
-      var temp = roundFactory.getInstance(10, None)
+      val roundFactory = RoundFactory()
+      val temp = roundFactory.getInstance(10, None)
       temp.siteID should be(10)
       temp.param should be(None)
     }
     "default routes 11" in {
-      val roundFactory = new RoundFactory
-      var temp = roundFactory.getInstance(11, None)
+      val roundFactory = RoundFactory()
+      val temp = roundFactory.getInstance(11, None)
       temp.siteID should be(11)
       temp.param should be(None)
     }
     "default routes 12" in {
-      val roundFactory = new RoundFactory
-      var temp = roundFactory.getInstance(12, None)
+      val roundFactory = RoundFactory()
+      val temp = roundFactory.getInstance(12, None)
       temp.siteID should be(12)
       temp.param should be(None)
     }
     "default routes 13" in {
-      val roundFactory = new RoundFactory
-      var temp = roundFactory.getInstance(13, None)
+      val roundFactory = RoundFactory()
+      val temp = roundFactory.getInstance(13, None)
       temp.siteID should be(13)
       temp.param should be(None)
     }
     "default routes 404" in {
-      val roundFactory = new RoundFactory
-      var temp = roundFactory.getInstance(404, None)
+      val roundFactory = RoundFactory()
+      val temp = roundFactory.getInstance(404, None)
       temp.siteID should be(0)
       temp.param should be(None)
     }
     "all again because of a bug from coveralls.io" in {
-      val roundFactory = new RoundFactory
+      val roundFactory = RoundFactory()
       var temp = roundFactory.getInstance(-1, None).siteID should be(-1)
       temp = roundFactory.getInstance(-1, None).param should be(None)
       temp = roundFactory.getInstance(0, None).siteID should be(0)
