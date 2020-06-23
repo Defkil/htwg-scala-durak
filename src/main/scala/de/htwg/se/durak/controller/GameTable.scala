cee9dc99287822ca86d123ad1a5b3c3c32c98d4c
Oliver <dev@olivergruettner.de>
2020-06-24 00:31:32 +0200

    fix index.html location again and cache apt in travis

--- src/main/scala/de/htwg/se/durak/controller/GameTable.scala
+++ src/main/scala/de/htwg/se/durak/controller/GameTable.scala
@@ -4,18 +4,18 @@ import de.htwg.se.durak.model.{CardDeck, Player}
 import scala.collection.mutable.ListBuffer
 
 case class GameTable() {
-  val CARDS_PER_PLAYER = 6
-  /*def createPlayerCardStack(players: List[Player]): List[CardStack] = {
-    val stack = new ListBuffer[CardStack]()
-    for(player <- players)
-      stack += new CardStack(0)
-    stack.toList
-  }
-  def handOutCards(cardStackList: List[CardStack], cardStack: CardStack): Unit = {
-    for(i <- cardStackList) {
-      for(j <- 0 until CARDS_PER_PLAYER) {
-        i.addCard(cardStack.popCard())
-      }
+/*val CARDS_PER_PLAYER = 6
+def createPlayerCardStack(players: List[Player]): List[CardStack] = {
+  val stack = new ListBuffer[CardStack]()
+  for(player <- players)
+    stack += new CardStack(0)
+  stack.toList
+}
+def handOutCards(cardStackList: List[CardStack], cardStack: CardStack): Unit = {
+  for(i <- cardStackList) {
+    for(j <- 0 until CARDS_PER_PLAYER) {
+      i.addCard(cardStack.popCard())
     }
-  }*/
+  }
+}*/
 }
