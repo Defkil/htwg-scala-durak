ca966c22db72ef22e724fa9c061f46ddb0d25cf8
Oliver Grüttner <dev@olivergruettner.de>
2021-01-24 16:01:58 +0100

    removed unused multiplayer option

--- src/main/scala/de/htwg/se/durak/controller/gameLogicComponent/GameLogicInterface.scala
+++ src/main/scala/de/htwg/se/durak/controller/gameLogicComponent/GameLogicInterface.scala
@@ -3,6 +3,4 @@ package de.htwg.se.durak.controller.gameLogicComponent
 trait GameLogicInterface {
   var get: GameStrategyInterface
   val menu: GameStrategyMenuInterface
-  def setLocalhost(): Unit
-  //def setMultiplayer(): Unit
 }
