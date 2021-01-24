cd14b59e8f1cf4ba102b6b8f7dc893318330b79b
Oliver Grüttner <dev@olivergruettner.de>
2021-01-24 13:52:45 +0100

    added more data to the FileIO test

--- src/test/scala/de/htwg/se/durak/model/fileIoComponent/fileIoJsonImpl/FileIOSpec.scala
+++ src/test/scala/de/htwg/se/durak/model/fileIoComponent/fileIoJsonImpl/FileIOSpec.scala
@@ -1,8 +1,7 @@
 package de.htwg.se.durak.model.fileIoComponent.fileIoJsonImpl
 
 import de.htwg.se.durak.model.fileIoComponent.fileIoJsonImpl
-import de.htwg.se.durak.model.gameElementsComponent.{CardDeckInterface, FieldInterface}
-import de.htwg.se.durak.model.gameElementsComponent.gameElementsBaseImpl.{CardDeck, Field, GameElements}
+import de.htwg.se.durak.model.gameElementsComponent.gameElementsBaseImpl.{Card, GameElements}
 import de.htwg.se.durak.model.playerComponent.Player
 import de.htwg.se.durak.model.roundComponent.{GameDataInterface, RoundDataInterface}
 import de.htwg.se.durak.model.roundComponent.roundBaseImpl.{GameData, Round, RoundData, TurnData}
@@ -10,18 +9,33 @@ import org.scalatest._
 
 class FileIOSpec extends WordSpec with Matchers{
   "FileIO JSON" should  {
-    "save file and load" in {
+    "save file and load without TurnData" in {
+      val gameDataList: List[GameDataInterface] = List(
+        GameData(
+          new RoundData(0, List("1")),
+          None
+        )
+      )
+
+      val fileIoJson = new fileIoJsonImpl.FileIO(GameElements(), new Round())
+      fileIoJson.save(gameDataList)
+
+      val res:List[GameDataInterface] = fileIoJson.load
+      res.toString() should equal("List(GameData(RoundData(0,List(\"1\"),None,Some(List(\"\"))),None))")
+    }
+
+    "save file and load with TurnData" in {
       val gameElements = new GameElements()
       val gameDataList: List[GameDataInterface] = List(
-        new GameData(
+        GameData(
           new RoundData(0, List("1")),
-          Option(new TurnData(
+          Option(TurnData(
             List(Player("PlayerA"), Player("PlayerB")),
-            List(),
+            List(gameElements.createCardDeck(List(new Card(5,1), new Card(6,1)))),
             0,
             1,
             gameElements.createField(),
-            gameElements.createCardDeck(),
+            gameElements.createCardDeck(List(new Card(4,1))),
             gameElements.createCardDeck(),
             1,
             0
@@ -29,12 +43,11 @@ class FileIOSpec extends WordSpec with Matchers{
         )
       )
 
-      val fileIoJson = new fileIoJsonImpl.FileIO(new GameElements(), new Round())
+      val fileIoJson = new fileIoJsonImpl.FileIO(GameElements(), new Round())
       fileIoJson.save(gameDataList)
 
       val res:List[GameDataInterface] = fileIoJson.load
-      println(res.toString())
-      res.toString() should equal("List(GameData(RoundData(0,List(\"1\"),None,Some(List(\"\"))),Some(TurnData(List(\"PlayerA\", \"PlayerB\"),List(),0,0,Field(CardDeck(List())),CardDeck(List()),CardDeck(List()),1,0))))")
+      res.toString() should equal("List(GameData(RoundData(0,List(\"1\"),None,Some(List(\"\"))),Some(TurnData(List(\"PlayerA\", \"PlayerB\"),List(CardDeck(List(Rang: 5, Typ: Pik, Rang: 6, Typ: Pik))),0,0,Field(CardDeck(List())),CardDeck(List(Rang: 4, Typ: Pik)),CardDeck(List()),1,0))))")
     }
   }
 }
