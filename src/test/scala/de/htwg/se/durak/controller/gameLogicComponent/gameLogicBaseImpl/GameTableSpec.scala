a8bc96fe3d6ef302a2189b374181a8bf5a44b2fb
Oliver Grüttner <dev@olivergruettner.de>
2021-01-24 22:43:49 +0100

    added test

--- src/test/scala/de/htwg/se/durak/controller/gameLogicComponent/gameLogicBaseImpl/GameTableSpec.scala
+++ src/test/scala/de/htwg/se/durak/controller/gameLogicComponent/gameLogicBaseImpl/GameTableSpec.scala
@@ -1,12 +1,29 @@
 package de.htwg.se.durak.controller.gameLogicComponent.gameLogicBaseImpl
 
-import de.htwg.se.durak.model.gameElementsComponent.gameElementsBaseImpl.GameElements
-import de.htwg.se.durak.model.roundComponent.roundBaseImpl.Round
+import de.htwg.se.durak.model.gameElementsComponent.gameElementsBaseImpl.{Card, GameElements}
+import de.htwg.se.durak.model.playerComponent.Player
+import de.htwg.se.durak.model.roundComponent.roundBaseImpl.{Round, TurnData}
 import org.scalatest._
 
 class GameTableSpec extends WordSpec with Matchers {
   "GameTable" should {
-
+    "add Spacer" in {
+      val gameTable = new GameTable(new GameElements, new Round)
+      val gameElements = new GameElements
+      val turnData = gameTable.addSpacer(TurnData(
+        List(Player("PlayerA"), Player("PlayerB")),
+        List(gameElements.createCardDeck(List(new Card(5,1), new Card(6,1)))),
+        0,
+        1,
+        gameElements.createField(),
+        gameElements.createCardDeck(List(new Card(4,1))),
+        gameElements.createCardDeck(),
+        1,
+        0
+      ))
+      turnData.field.cardDeck.deck.last.rank should be(-1)
+      turnData.field.cardDeck.deck.last.symbol should be(-1)
+    }
   }
 
   "test unapply" in {
