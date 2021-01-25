66a4c4c7ba10e63d4dfd3b7642694128c074d8a8
Oliver Grüttner <dev@olivergruettner.de>
2021-01-25 02:46:33 +0100

    added test

---
+++ src/test/scala/de/htwg/se/durak/controller/gameLogicComponent/gameLogicBaseImpl/GameStrategySpec.scala
@@ -0,0 +1,34 @@
+package de.htwg.se.durak.controller.gameLogicComponent.gameLogicBaseImpl
+
+import de.htwg.se.durak.model.gameElementsComponent.gameElementsBaseImpl.{Card, GameElements}
+import de.htwg.se.durak.model.playerComponent.Player
+import de.htwg.se.durak.model.roundComponent.{GameDataInterface, TurnDataInterface}
+import de.htwg.se.durak.model.roundComponent.roundBaseImpl.{GameData, Round, RoundData, TurnData}
+import org.scalatest._
+
+class GameStrategySpec extends WordSpec with Matchers {
+  "GameStrategy" should {
+    "nextTurn 0-3" in {
+      val gameElements = new GameElements
+      val gameStrategy = new GameStrategy(gameElements, new Round)
+      val turnData: TurnDataInterface = TurnData (
+        List(Player("PlayerA"), Player("PlayerB")),
+        List(gameElements.createCardDeck(List(new Card(5,1), new Card(6,1)))),
+        0,
+        1,
+        gameElements.createField(gameElements.createCardDeck(List(new Card(4,1)))),
+        gameElements.createCardDeck(List(new Card(4,2))),
+        gameElements.createCardDeck(),
+        1,
+        0
+      )
+      var gameData: GameDataInterface = new GameData(new RoundData(0,List("")), Option(turnData))
+      gameData = gameStrategy.nextTurn(gameData, "")
+    }
+
+    "test unapply" in {
+      val gameStrategy = new GameStrategy(new GameElements, new Round)
+      GameStrategy.unapply(gameStrategy)
+    }
+  }
+}
