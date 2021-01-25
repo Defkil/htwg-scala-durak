07f3b0cb0b25b696b7c963f6279f773dcc4be9ee
Oliver Grüttner <dev@olivergruettner.de>
2021-01-25 02:51:14 +0100

    added test

--- src/test/scala/de/htwg/se/durak/controller/gameLogicComponent/gameLogicBaseImpl/GameStrategySpec.scala
+++ src/test/scala/de/htwg/se/durak/controller/gameLogicComponent/gameLogicBaseImpl/GameStrategySpec.scala
@@ -24,6 +24,26 @@ class GameStrategySpec extends WordSpec with Matchers {
       )
       var gameData: GameDataInterface = new GameData(new RoundData(0,List("")), Option(turnData))
       gameData = gameStrategy.nextTurn(gameData, "")
+      gameData.roundData.siteID should be(11)
+    }
+
+    "nextTurn 1-1" in {
+      val gameElements = new GameElements
+      val gameStrategy = new GameStrategy(gameElements, new Round)
+      val turnData: TurnDataInterface = TurnData (
+        List(Player("PlayerA"), Player("PlayerB")),
+        List(gameElements.createCardDeck(List(new Card(5,1), new Card(6,1)))),
+        0,
+        1,
+        gameElements.createField(gameElements.createCardDeck(List(new Card(4,1)))),
+        gameElements.createCardDeck(List(new Card(4,2))),
+        gameElements.createCardDeck(),
+        1,
+        1
+      )
+      var gameData: GameDataInterface = new GameData(new RoundData(0,List("")), Option(turnData))
+      gameData = gameStrategy.nextTurn(gameData, "")
+      gameData.roundData.siteID should be(11)
     }
 
     "test unapply" in {
